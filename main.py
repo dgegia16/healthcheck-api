0fab95c727dd497c3ac2e48661693c4caaa1a91b
Dati <dgegia16@gmail.com>
2026-05-20 23:55:13 +0400

    added root and health endpoint

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from fastapi import FastAPI
+import psutil
 
 app = FastAPI()
 
 
+@app.get("/")
+def root():
+    return {"message": "Hello from HealthCheck API"}
+
+@app.get("/health")
+def health():
+
+    cpu = psutil.cpu_percent()
+    memory = psutil.virtual_memory().percent
+    disk = psutil.disk_usage("/").percent
+
+    if cpu > 90 or memory > 90 or disk > 90:
+        status = "unhealthy"
+    elif cpu > 70 or memory > 70 or disk > 70:
+        status = "degraded"
+    else:
+        status = "healthy"
+
+
+
+
+    return {
+        "status": status,
+        "cpu_percent": cpu,
+        "virtual_memory": memory,
+        "disk_usage": disk
+    }
